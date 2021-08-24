89e0192c15de079db1a7f556fe30b9a031b58725
Malek Mousa <malek.mousa1407@gmail.com>
2021-08-24 19:43:45 +0400

    Commit.

--- src/components/ReviewScreen.js
+++ src/components/ReviewScreen.js
@@ -23,14 +23,14 @@ function ReviewScreen() {
 
     // Fetch Tasks
     const fetchTasks = async() => {
-      const res = await fetch('http://localhost:5000/tasks');
+      const res = await fetch('http://localhost:6000/tasks');
       const data = await res.json();
       return data; 
     }    
 
     // Fetch task
     const fetchTask = async(id) => {
-      const res = await fetch(`http://localhost:5000/tasks/${id}`);
+      const res = await fetch(`http://localhost:6000/tasks/${id}`);
       const data = await res.json();
       return data; 
     } 
@@ -39,7 +39,7 @@ function ReviewScreen() {
     const addTask = async (task) => {
 
       const res = await fetch(
-        'http://localhost:5000/tasks',{
+        'http://localhost:6000/tasks',{
           method: 'POST',
           headers: {
             'Content-type' : 'application/json'
@@ -58,7 +58,7 @@ function ReviewScreen() {
 
     // Delete task
     const deleteTask = async (id) =>{
-      await fetch(`http://localhost:5000/tasks/${id}`, {
+      await fetch(`http://localhost:6000/tasks/${id}`, {
         method: 'DELETE'
       })
 
@@ -70,7 +70,7 @@ function ReviewScreen() {
       const taskToToggle = await fetchTask(id)
       const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
   
-      const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      const res = await fetch(`http://localhost:6000/tasks/${id}`, {
         method: 'PUT',
         headers: {
           'Content-type': 'application/json',
