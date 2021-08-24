4a854e3d96052880c540b61447c6ff9f69392955
Malek Mousa <malek.mousa1407@gmail.com>
2021-08-24 20:23:37 +0400

    Added Review Page.

--- src/components/ReviewScreen.js
+++ src/components/ReviewScreen.js
@@ -1,115 +1,32 @@
-import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import React from "react";
-import Header from "./ReviewScreen/Header";
-import Footer from "./ReviewScreen/Footer";
-import Tasks from "./ReviewScreen/Tasks";
-import AddTask from "./ReviewScreen/AddTask";
-import About from "./ReviewScreen/About";
+import React, { useState } from "react";
+import Form from "./ReviewScreen/Form";
+import "../App.css";
+import ReviewList from "./ReviewScreen/ReviewList";
+import {uuid} from 'uuidv4'
 
 function ReviewScreen() {
-
-  const [showAddTask, setShowAddTask] = useState(false);
-
-  const [tasks, setTasks] = useState([]);
-
-    useEffect(() => { 
-      const getTasks = async () => {
-        const tasksFromServer = await fetchTasks();
-        setTasks(tasksFromServer)
-      }
-      getTasks();
-    },[]);
-
-    // Fetch Tasks
-    const fetchTasks = async() => {
-      const res = await fetch('http://localhost:6000/tasks');
-      const data = await res.json();
-      return data; 
-    }    
-
-    // Fetch task
-    const fetchTask = async(id) => {
-      const res = await fetch(`http://localhost:6000/tasks/${id}`);
-      const data = await res.json();
-      return data; 
-    } 
-
-    // Add Task
-    const addTask = async (task) => {
-
-      const res = await fetch(
-        'http://localhost:6000/tasks',{
-          method: 'POST',
-          headers: {
-            'Content-type' : 'application/json'
-          },
-          body: JSON.stringify(task)
-        });
-
-        const data = await res.json();
-
-        setTasks([...tasks,data]);
-
-      // const id = Math.floor(Math.random() * 10_000) + 1;
-      // const newTask = {id,...task}
-      // setTasks([...tasks, newTask]);
-    }
-
-    // Delete task
-    const deleteTask = async (id) =>{
-      await fetch(`http://localhost:6000/tasks/${id}`, {
-        method: 'DELETE'
-      })
-
-      setTasks(tasks.filter(task => task.id !== id));
-    };
-
-    // Toggle reminder
-    const toggleReminder = async (id) => {
-      const taskToToggle = await fetchTask(id)
-      const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
-  
-      const res = await fetch(`http://localhost:6000/tasks/${id}`, {
-        method: 'PUT',
-        headers: {
-          'Content-type': 'application/json',
-        },
-        body: JSON.stringify(updTask),
-      })
-  
-      const data = await res.json()
-  
-      setTasks(
-        tasks.map((task) =>
-          task.id === id ? { ...task, reminder: data.reminder } : task
-        )
-      )
-    }
+  const [reviews, setReviews] = useState([]);
+  const [form, setForm] = useState({ university: "", review: "", id: uuid() });
+  const [editing, setEditing] = useState(false);
 
   return (
-    <Router>
-    <div className='container'>
-        <Header
-          onAdd={() => setShowAddTask(!showAddTask)}
-          showAdd={showAddTask}/>
-        <Route
-          path='/'
-          exact
-          render={(props) => (
-            <>
-              {showAddTask && <AddTask onAdd={addTask} />}
-              {tasks.length > 0 ? (<Tasks
-              tasks={tasks}
-              onDelete={deleteTask}
-              onToggle={toggleReminder}/>) : ('No Tasks To Show')}
-            </>
-          )}
-          />
-        <Route path='/about' component={About} />      
-      </div>
-      <Footer/>
-  </Router>
+    <div className="app">
+      <h1>Internship Reviews</h1>
+      <Form
+        editing={editing}
+        form={form}
+        reviews={reviews}
+        setEditing={setEditing}
+        setForm={setForm}
+        setReviews={setReviews}
+      />
+      <ReviewList
+        reviews={reviews}
+        setForm={setForm}
+        setEditing={setEditing}
+        setReviews={setReviews}
+      />
+    </div>
   );
 }
 
